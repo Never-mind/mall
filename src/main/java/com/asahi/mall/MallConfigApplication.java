a4ede14e49a290692a7197d17da16a5c3c430b2e
Never-mind <542732342@qq.com>
2020-03-09 02:25:33 +0800

    spring+mybatis

---
+++ src/main/java/com/asahi/mall/MallConfigApplication.java
@@ -0,0 +1,13 @@
+package com.asahi.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallConfigApplication.class, args);
+	}
+
+}
